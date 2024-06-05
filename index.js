8feb6d55e95144e746372f2580030d88063e1e4f
MCavieres <maca.c.91@gmail.com>
2024-06-05 10:40:17 -0400

    aplicacion de filtros

--- index.js
+++ index.js
@@ -5,9 +5,59 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-    console.log(guitarras);
+app.get("/api/v2", async (req, res) => {
     res.send(guitarras);
 });
 
-app.listen(3000, () => console.log("Servidor encendido!"));
+const hateoas = (guitarras) => {
+    return guitarras.map((item) => {
+        return {
+            name: item.name,
+            href: `http://localhost:3000/guitarra/${item.id}`,
+        };
+    });
+};
+
+app.get("/api/v2/guitarras", (req, res) => {
+    res.json({ guitarras: hateoas(guitarras) });
+});
+
+const guitarra = (id) => {
+    return guitarras.find((item) => item.id === +id);
+};
+
+app.get("/api/v2/guitarras/:id", (req, res) => {
+    const { id } = req.params;
+
+    const guitar = guitarra(id);
+
+    if (guitar) {
+        res.json(guitar);
+    } else {
+        res.json({ msg: "guitarra no encontrada" }).status(404);
+    }
+});
+
+const filtroByBody = (body) => {
+    return guitarras.filter((item) => item.body === body);
+};
+
+app.get("/api/v2/body/:cuerpo", (req, res) => {
+    const { cuerpo } = req.params;
+    res.send({
+        cant: filtroByBody(cuerpo).length,
+        guitarras: filtroByBody(cuerpo),
+    });
+});
+
+const orderValues = (order) => {
+    return order === "asc"
+        ? guitarras.sort((a, b) => a.value - b.value)
+        : order === "desc"
+        ? guitarras.sort((a, b) => b.value - a.value)
+        : false;
+};
+
+app.get("/api/v2/guitarras/:value", (req, res) => {});
+
+app.listen(3000, () => console.log("Servidor encendido!", `http://localhost:3000`));
